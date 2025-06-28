fd671c611d3257aae2595cd3e63059e84b222198
J Ashwin Rao <jashwinrao31@gmail.com>
2025-06-28 09:55:30 +0530

    added some controllers, routes, models and middlewares

--- Server/server.js
+++ Server/server.js
@@ -4,8 +4,13 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
+import userRouter from './routes/userRoutes.js';
+import hotelRouter from './routes/hotelRoutes.js';
+import connectCloudinary from './config/cloudinary.js';
+import roomRouter from './routes/roomRouter.js';
 
 connectDB()
+connectCloudinary()
 const app = express()
 app.use(cors());
 
@@ -17,8 +22,11 @@ app.use(clerkMiddleware())
 app.use("/api/clerk", clerkWebhooks)
 
 app.get('/', (req, res) => {
-    res.send('API is working')
+    res.send('API is workingl')
 })
+app.use('/api/user', userRouter)
+app.use('/api/hotels', hotelRouter)
+app.use('/api/rooms', roomRouter)
 
 const PORT = process.env.PORT || 3000;
 
